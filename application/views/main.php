b090055a9d9d78d6601bdbb06114f4eb873ca7fe
kuriyantoadi <melatikuning212@gmail.com>
2020-03-22 15:23:55 +0700

    update

---
+++ application/views/main.php
@@ -0,0 +1,7 @@
+<html>
+<title>HTML Tutorial</title>
+<body>
+<h1>This is a heading</h1>
+<?php echo anchor('C_buku/input','Tambah data') ?>
+</body>
+</html>
---
+++ application/views/v_tampil.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <?php
+    echo form_open('C_buku/tambah');
+    echo anchor('C_buku/input','tambah');
+    ?>
+
+    <table border="1">
+      <tr>
+        <td>No</td>
+        <td>Judul</td>
+        <td>Pengarang</td>
+        <td>Tahun Terbit</td>
+        <td>Edit</td>
+      </tr>
+      <?php
+        $no=1;
+        foreach ($buku as $row) {
+       ?>
+      <tr>
+        <td><?php echo $no++; ?></td>
+        <td><?php echo $row->judul; ?></td>
+        <td><?php echo $row->pengarang; ?></td>
+        <td><?php echo $row->tahun_terbit ?></td>
+        <td><?php echo anchor('C_buku/e/'.$row->id_buku,'Edit');  ?></td>
+      </tr>
+    <?php echo form_close(); } ?>
+    </table>
+  </body>
+</html>
